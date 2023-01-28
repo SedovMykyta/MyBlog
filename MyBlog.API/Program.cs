3eba426d2f21a724b4bfadee1ed4dd7174336a11
Nekitosn <nikitasedov57@gmail.com>
2023-01-28 22:44:31 +0200

    fix - Fixed code review remarks.

--- MyBlog.API/Program.cs
+++ MyBlog.API/Program.cs
@@ -3,21 +3,16 @@ using MyBlog.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<MyBlogContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyBlogDatabase")));
 
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
