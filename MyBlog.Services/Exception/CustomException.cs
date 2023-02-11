a9052f7db8f956ad464a87fd183f0fee45da1218
Nekitosn <nikitasedov57@gmail.com>
2023-02-11 02:07:26 +0200

    fix - Fixed review remarks.

--- MyBlog.Services/Exception/CustomException.cs
+++ MyBlog.Services/Exception/CustomException.cs
@@ -5,7 +5,6 @@ namespace MyBlog.Service.Exception;
 public abstract class CustomException : System.Exception
 {
     public HttpStatusCode StatusCode { get; }
-    
     public object? ResponseAdditionalInfo { get; }
 
     protected CustomException(string message, HttpStatusCode statusCode, object? responseAdditionalInfo = null) 
